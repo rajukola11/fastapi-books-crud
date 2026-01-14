4da7689e3c9612e0d5a5742e2fb543fd2233f00b
Raju Kola <rajukola754@gmail.com>
2026-01-14 15:44:19 +0530

    added update and delete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,status,HTTPException
 from pydantic import BaseModel
-from typing import List
+from typing import List,Optional
 
 app = FastAPI()
 
@@ -52,18 +52,28 @@ class Book(BaseModel):
     page_count:int
     language:str
 
+class UpdateBook(BaseModel):
+    title:Optional[str]=None
+    author:Optional[str]=None
+    publisher:Optional[str]=None
+    language:Optional[str]=None
+
 @app.get("/books",response_model=List[Book])
 def get_all_books():
     return books
 
 @app.post("/book",status_code=status.HTTP_201_CREATED)
-def create_a_book(book_data:Book)->dict:
-    new_book = book_data.model_dump()
-    books.append(new_book)
-    return new_book
+def create_a_book(book_data:Book):
+    if any(book["id"] == book_data.id for book in books):
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Book with this ID already exists"
+        )
+    books.append(book_data.model_dump())    
+    return book_data
 
 @app.get("/book/{book_id}",response_model=Book)
-def get_a_book(book_id:int)->dict:
+def get_a_book(book_id:int)->Book:
     for book in books:
         if book['id']==book_id:
             return book
@@ -72,4 +82,28 @@ def get_a_book(book_id:int)->dict:
         detail="Book not found"
     )
 
-    
+@app.patch("/book/{book_id}",response_model=Book)
+def update_a_book(book_id:int,update_book:UpdateBook)->Book:
+    for book in books:
+        if book['id']==book_id:
+            for key,value in update_book.model_dump(exclude_unset=True).items():
+                book[key]=value
+            return book
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Book not found"
+    )
+
+@app.delete("/book/{book_id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_a_book(book_id:int):
+    for book in books:
+        if book['id']==book_id:
+            books.remove(book)
+            return 
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Book not found"
+    )
+
+
